7881f0e7e8d80dbbf92703c5e89514da26f30024
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-06-07 18:19:30 +0100

    added all review features on review page

--- fe/pages/reviews/[slug].js
+++ fe/pages/reviews/[slug].js
@@ -1,15 +1,53 @@
-import React from "react";
+import React from 'react';
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css';
 
 import axios from 'axios';
 
 export default function Review({ review, product }) {
-
-    console.log({ review, product });
     return (
-        <div>
-            <h1>{product.name}</h1>
-            <h1>{review.title}</h1>
-            <h2>{review.subtitle}</h2>
+        <div className={styles.container}>
+            <Head>
+                <title>{`Review: ${product.name} - ${review.title}`}Yokupoku - Reviews for people with short attention span</title>
+                <meta lang="en" />
+                <meta charSet="utf-8" />
+                <meta name="description" content="Yokupoku - Mayoku - reviews for people with short attention span" />
+                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+                <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+            </Head>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    {product.name}
+                </h1>
+                <h1>{review.title}</h1>
+                <h2>{review.subtitle}</h2>
+                <p className={styles.description}>
+                    {review.content}
+                </p>
+                <p>
+                    <h3>Pros</h3>
+                    {review.pros}
+                </p>
+                <p>
+                    <h3>Cons</h3>
+                    {review.cons}
+                </p>
+
+                <p>
+                    <h3>Tags</h3>
+                    {review.tags}
+                </p>
+                <p>Suggested: {review.suggested ? 'YES' : 'NO'}</p>
+                <p>
+                    <h3>Boredom Speed Index</h3>
+                    <h2>{review.bsi}</h2>
+                </p>
+                <p>
+                    <h3>Rate</h3>
+                    <h2>{review.rating} / 100</h2>
+                </p>
+                {/* Maybe can add Updated/Created */}
+            </main>
         </div>
     );
 }

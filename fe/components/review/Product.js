ccea2fb599c9d727226c1d06a59009fb53fa718f
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-06-09 18:58:40 +0100

    adding info to product

--- fe/components/review/Product.js
+++ fe/components/review/Product.js
@@ -1,10 +1,14 @@
+import ProductType from '../common/ProductType';
 import styles from './styles/Product.module.css';
 
 const Product = ({ product }) => {
-    const { name } = product;
+    const { name, type, meta, links } = product;
     return (
         <div className={styles.wrapper}>
+            <h2><ProductType type={type} /></h2>
             <h1>{name}</h1>
+            {meta?.store && <h3>🛒: {meta?.store}</h3>}
+            {links?.map((l, i) => <h3>{l}</h3>)}
         </div>
     );
 };

2c623bf145f15ad9302b773cd0d50bf2ac7d138e
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-09-07 21:27:53 +0100

    adding basic SEO support

---
+++ fe/next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+    openGraph: {
+      type: 'website',
+      locale: 'en_GB',
+      url: 'https://yokupoku.surge.sh/',
+      site_name: 'Yokupoku',
+    },
+    twitter: {
+      handle: '@vikkio88',
+      site: '@vikkio88',
+      cardType: 'summary_large_image',
+    },
+  };
--- fe/pages/_app.js
+++ fe/pages/_app.js
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import 'react-tippy/dist/tippy.css'
+import { Container } from 'next/app';
+import { DefaultSeo } from 'next-seo';
+
+import seo from '../next-seo.config';
+
+import '../styles/globals.css';
+import 'react-tippy/dist/tippy.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <DefaultSeo
+        {...seo}
+      />
+      <Component {...pageProps} />;
+    </Container>
+  );
 }
 
-export default MyApp
+export default MyApp;

e36fc0de405788bc3d8365d7281172b091fc4342
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2022-10-27 16:02:54 +0100

    fixing reviews order

--- be/models/reviews.js
+++ be/models/reviews.js
@@ -87,7 +87,7 @@ module.exports = {
         const result = await db(TABLES.REVIEWS).select('reviews.*',
             'products.name as productName', 'products.type as productType', 'products.slug as productSlug'
         ).innerJoin(TABLES.PRODUCTS, 'products.id', '=', 'reviews.productId')
-            .where('published', true).orderBy('updatedAt', 'desc')
+            .where('published', true).orderBy('createdAt', 'desc')
             .then(rows => rows.map(format.select));
         return result;
     },

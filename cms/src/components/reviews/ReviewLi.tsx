c4aea022de0f3c7327aca2fc266b4d51b43cd4f9
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2024-12-30 14:08:39 +0000

    finished reviews list

--- cms/src/components/reviews/ReviewLi.tsx
+++ cms/src/components/reviews/ReviewLi.tsx
@@ -1,9 +1,26 @@
 import { ReviewLi as ListItem } from "yokupoku-shared";
 import s from "./styles/reviewLi.module.css";
+import { useNavigate } from "react-router";
 
 type Props = {
   review: ListItem;
 };
 export default function ReviewLi({ review }: Props) {
-  return <li className={s.li}>{review.title}</li>;
+  const navigate = useNavigate();
+  return (
+    <li className={`${s.li} f r spb ic`}>
+      <p className="f c">
+        <h3>{review.title}</h3>
+        <span>{review.product.name}</span>
+      </p>
+      <span>
+        <button
+          className="small"
+          onClick={() => navigate(`/reviews/${review.id}`)}
+        >
+          🔎
+        </button>
+      </span>
+    </li>
+  );
 }

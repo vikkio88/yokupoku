d93b6bfca1ced213642577d7706cc290987c2089
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2025-10-02 21:04:42 +0100

    more ts migration

---
+++ be/libs/utils.ts
@@ -0,0 +1,37 @@
+const { ulid } = require("ulid");
+
+export const slugify = (text: string) =>
+  text
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w\-]+/g, "")
+    .replace(/\-\-+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
+
+export const generateId = () => {
+  return ulid();
+};
+
+export const nBoolean = (value: any) =>
+  value === null ? null : Boolean(value);
+
+export const now = () => new Date().toUTCString();
+
+// comma separated list
+export const csl = {
+  toString(list: string[] | string | null | undefined) {
+    if (Array.isArray(list)) return list.join(",");
+    if (typeof list === "string") {
+      const splitted = new Set(this.fromString(list));
+      return Array.from(splitted).join(",");
+    }
+    return "";
+  },
+  fromString(list: string) {
+    return list
+      .trim()
+      .replace(/\s?,\s?/g, ",")
+      .split(",");
+  },
+};

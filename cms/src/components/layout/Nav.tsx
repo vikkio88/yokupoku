c2e6b5c4a7e801116666180cc473a2ef8a96d334
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2025-10-06 20:08:10 +0100

    removed home

--- cms/src/components/layout/Nav.tsx
+++ cms/src/components/layout/Nav.tsx
@@ -1,7 +1,6 @@
 export default function Nav() {
   return (
     <nav className="f r g">
-      <a href="/">Home</a>
       <a href="/products">Products</a>
       <a href="/reviews">Reviews</a>
     </nav>
